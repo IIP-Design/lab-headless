6b6bda4707e1b9d74dade3b97362bb52977aec0a
mrewers <mrewers@users.noreply.github.com>
2020-10-14 16:16:27 -0400

    feat(webroot): add lab-guillotine plugin to the composer file

--- .devcontainer/scripts/build-site.sh
+++ .devcontainer/scripts/build-site.sh
@@ -5,11 +5,17 @@ echo "\xF0\x9F\x92\xBE Cloning the webroot into the container...\n"
 
 docker cp ../webroot/. lab_web:$DIR/
 
-# Delete the Google login and hide login plugins, since neither is needed for development
 echo "\xF0\x9F\xA7\xB9 Removing plugins not needed for development.\n"
 
+# Delete the Google login and hide login plugins, since neither is needed for development
 docker exec -i lab_web sed -i '/wpackagist-plugin\/google-apps-login/d' $DIR/composer.json
 docker exec -i lab_web sed -i '/iip-design\/wp-hide-login-form/d' $DIR/composer.json
+
+# Delete plugins that are part of the monorepo
+docker exec -i lab_web sed -i '/gpalab\/lab-guillotine/d' $DIR/composer.json
+
+# Delete all the repository references to monorepo
+docker exec -i lab_web sed -i '/"type": "path"/I,+3 d' $DIR/composer.json
 
 # Run Composer to install WordPress and all the site dependencies
 echo "\xE2\x8F\xB1  Installing site dependencies with Composer, this may take a few minutes...\n"
