a31f6be37b8c4341c618d5aa18a5ce1bc6b0d98b
mrewers <mrewers@users.noreply.github.com>
2020-12-14 17:35:26 -0500

    feat(guillotine): adjust event post template, adding event date

--- plugins/lab-guillotine/custom-post-types/event/single-gpalab_event.php
+++ plugins/lab-guillotine/custom-post-types/event/single-gpalab_event.php
@@ -21,7 +21,38 @@ get_header();
     while ( have_posts() ) {
       the_post();
 
-      get_template_part( 'template-parts/content', get_post_type() );
+      ?>
+      <article class="gpalab-event" id="post-<?php the_ID(); ?>">
+
+        <figure class="featured-media gpalab-featured-image">
+          <div class="featured-media-inner section-inner">
+            <?php the_post_thumbnail(); ?>
+          </div>
+        </figure>
+
+        <div class="post-inner thin">
+          <div class="entry-content">
+            <h2><?php the_title(); ?></h2>
+
+            <?php
+            $date = get_post_meta( get_the_ID(), '_gpalab_event_date', true );
+
+            if ( ! empty( $date ) ) {
+              $date_format = get_option( 'date_format' );
+              $time_format = get_option( 'time_format' );
+              $format      = $date_format . ' ' . $time_format . ' T';
+
+              $formatted = date_i18n( $format, strtotime( $date ) );
+              echo '<p><strong>' . esc_html( __( 'When', 'gpalab-guillotine' ) ) . ': </strong>' . esc_html( $formatted ) . '<p>';
+            }
+
+            the_content();
+            ?>
+          </div>
+        </div>
+
+      </article>
+      <?php
     }
   }
 
