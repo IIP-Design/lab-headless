49cf5561d64bbea6c9232315a5621ea91f62baac
mrewers <mrewers@users.noreply.github.com>
2021-01-10 23:36:40 -0500

    feat: add placeholder classes for custom gql types

---
+++ plugins/lab-guillotine/graphql/types/class-docs-hub-gql.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Registers the Docs_Hub_GQL class.
+ *
+ * @package GPALAB_Headless\Guillotine
+ * @since 0.0.1
+ */
+
+namespace Guillotine;
+
+/**
+ * Describes the schema for the docs hub data.
+ *
+ * @package GPALAB_Headless\Guillotine
+ * @since 0.0.1
+ */
+class Docs_Hub_GQL {}
