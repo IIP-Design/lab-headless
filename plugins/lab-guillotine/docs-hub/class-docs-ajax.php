9fabcd8c43a2c03a5770b3eefa851ddafc57815b
mrewers <mrewers@users.noreply.github.com>
2021-01-11 19:49:01 -0500

    feat(docs-hub): add nonce validation to save docs AJAX request

--- plugins/lab-guillotine/docs-hub/class-docs-ajax.php
+++ plugins/lab-guillotine/docs-hub/class-docs-ajax.php
@@ -22,12 +22,22 @@ class Docs_Ajax {
    * @since 0.0.1
    */
   public function handle_add_repo() {
-    $data = json_decode( wp_unslash( $_POST['data'] ), true );
+    // The following rules are handled by the gpalab_verify_nonce function and hence can be safely ignored.
+    // phpcs:disable WordPress.Security.NonceVerification.Missing
+    // phpcs:disable WordPress.Security.ValidatedSanitizedInput.InputNotSanitized
+    // phpcs:disable WordPress.Security.ValidatedSanitizedInput.MissingUnslash
+    // phpcs:disable WordPress.Security.ValidatedSanitizedInput.InputNotValidated
+    $this->gpalab_verify_nonce( $_POST['security'], 'gpalab_docs_hub_save' );
 
+    $data = ! empty( $_POST['data'] ) ? json_decode( wp_unslash( $_POST['data'] ), true ) : array();
+    // phpcs:enable
+
+    // Construct the reference string for the repository source.
     $parent = ! empty( $data['repository'] ) ? $this->construct_parent_name( $data['repository'] ) : '';
 
     // TODO: add parents to array in options called gpalab_guillotine_docs_hub_repos.
 
+    // Iterate over the repo's doc pages, adding each to the DB.
     if ( ! empty( $data['files'] ) && is_array( $data['files'] ) ) {
 
       foreach ( $data['files'] as $file ) {
@@ -38,7 +48,7 @@ class Docs_Ajax {
   }
 
   /**
-   * Cobbles together
+   * Cobbles together a reference string for a given repository source.
    *
    * @param object $repo_data    The parts of a repository's path.
    * @return string              The combined path parts.
@@ -86,5 +96,32 @@ class Docs_Ajax {
     // phpcs:disable WordPress.DB.PreparedSQL.NotPrepared
     $wpdb->query( $query );
     // phpcs:enable
+  }
+
+  /**
+   * Checks that a security nonce is set, valid, and from a permitted referrer.
+   *
+   * @param string $security   A nonce provided in the Ajax call.
+   * @param string $expected   The expected nonce value.
+   *
+   * @since 0.0.1
+   */
+  private function gpalab_verify_nonce( $security, $expected ) {
+    $nonce = null;
+
+    // Make sure nonce is set.
+    if ( ! isset( $security ) ) {
+      return;
+    } else {
+      $nonce = sanitize_text_field( wp_unslash( $security ) );
+    }
+
+    // Verify the nonce.
+    if (
+      wp_verify_nonce( $nonce, $expected ) === false ||
+      check_ajax_referer( $expected, 'security', false ) === false
+    ) {
+      return;
+    }
   }
 }
