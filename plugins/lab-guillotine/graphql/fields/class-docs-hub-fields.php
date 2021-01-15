186c18ac769e18595df44edabd535019856cdf62
mrewers <mrewers@users.noreply.github.com>
2021-01-15 16:35:35 -0500

    feat(gql): add resolve for docs pages to the root query

--- plugins/lab-guillotine/graphql/fields/class-docs-hub-fields.php
+++ plugins/lab-guillotine/graphql/fields/class-docs-hub-fields.php
@@ -26,12 +26,60 @@ class Docs_Hub_Fields {
       'RootQuery',
       'gpalabDocsPages',
       array(
-        'description' => __( 'A documentation page connected in the Docs Hub', 'gpalab-guillotine' ),
-        'resolve'     => function() {
-          return 'world';
+        'description' => __( 'A list of documentation pages connected in the Docs Hub', 'gpalab-guillotine' ),
+        'args'        => array(
+          'parent' => array(
+            'description' => __( 'Specify page source in the format "owner/repo/sub-directory@branch"', 'gpalab-guillotine' ),
+            'type'        => array( 'non_null' => 'String' ),
+            'required'    => true,
+          ),
+        ),
+        'resolve'     => function( $root, $args ) {
+          return $this->gpalab_docs_pages_resolver( $root, $args );
         },
-        'type'        => 'String',
+        'type'        => array( 'list_of' => 'gpalabDocsPage' ),
       )
     );
+  }
+
+  /**
+   * Resolve queries for documentation pages based on the parent string.
+   *
+   * @param null  $root  The previous object/array was being resolved. Our field is at the Root, so it’s passed a null value.
+   * @param array $args  The input values from provided by the user to be passed into the query.
+   * @return array       Results from the database query.
+   *
+   * @since 0.0.1
+   */
+  private function gpalab_docs_pages_resolver( $root, $args ) {
+    global $wpdb;
+
+    // phpcs:disable WordPress.DB.DirectDatabaseQuery.DirectQuery
+    // phpcs:disable WordPress.DB.DirectDatabaseQuery.NoCaching
+    $results = $wpdb->get_results(
+      $wpdb->prepare(
+        "SELECT * FROM {$wpdb->prefix}gpalab_docs_hub WHERE `parent` = %s",
+        $args['parent']
+      )
+    );
+    // phpcs:enable
+
+    $transformed = array();
+
+    if ( ! empty( $results ) ) {
+
+      foreach ( $results as $result ) {
+        $item['pageContent'] = $result->page_content;
+        $item['pageId']      = $result->page_id;
+        $item['pageName']    = $result->page_name;
+        $item['pageOID']     = $result->page_oid;
+        $item['pagePath']    = $result->page_path;
+        $item['parent']      = $result->parent;
+
+        array_push( $transformed, $item );
+      }
+    }
+
+    return $transformed;
   }
 }
