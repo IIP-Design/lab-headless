5d83ee3ae3bc59741226c7e7283a4d1a2557af68
mrewers <mrewers@users.noreply.github.com>
2021-01-19 17:07:53 -0500

    fix(front): correct reference error in MoreStories component that was blocking builds

--- frontend/components/MoreStories/MoreStories.js
+++ frontend/components/MoreStories/MoreStories.js
@@ -1,24 +1,42 @@
+import propTypes from 'prop-types';
+
 import PostPreview from '../PostPreview/PostPreview';
 
-const MoreStories = ( { posts } ) => (
-  <section>
-    <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-      More Stories
-    </h2>
-    <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
-      { posts.map( ( { node } ) => (
-        <PostPreview
-          key={ node.slug }
-          title={ node.title }
-          coverImage={ node.featuredImage.node }
-          date={ node.date }
-          author={ node.author.node }
-          slug={ node.slug }
-          excerpt={ node.excerpt }
-        />
-      ) ) }
-    </div>
-  </section>
-);
+const MoreStories = ( { posts } ) => {
+  if ( posts ) {
+    return (
+      <section>
+        <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
+          More Stories
+        </h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 md:col-gap-16 lg:col-gap-32 row-gap-20 md:row-gap-32 mb-32">
+          { posts.map( ( { node } ) => {
+            if ( node ) {
+              return (
+                <PostPreview
+                  key={ node?.slug }
+                  title={ node?.title }
+                  coverImage={ node?.featuredImage?.node }
+                  date={ node?.date }
+                  author={ node?.author?.node }
+                  slug={ node?.slug }
+                  excerpt={ node?.excerpt }
+                />
+              );
+            }
+
+            return null;
+          } ) }
+        </div>
+      </section>
+    );
+  }
+
+  return null;
+};
+
+MoreStories.propTypes = {
+  posts: propTypes.array,
+};
 
 export default MoreStories;
