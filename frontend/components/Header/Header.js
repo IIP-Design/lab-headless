286d2d264880ee7e4c312940db8104accfb9be4f
mrewers <mrewers@users.noreply.github.com>
2021-01-22 17:23:06 -0500

    feat(front): add nav bar to the header

--- frontend/components/Header/Header.js
+++ frontend/components/Header/Header.js
@@ -5,12 +5,20 @@ import style from './Header.module.scss';
 const Header = () => (
   <div className={ style.header }>
     <Link href="/">
-      <img
-        alt="U.S. Department of State seal"
-        className={ style.logo }
-        src="/dos-seal.png"
-      />
+      <div className={ style.branding }>
+        <img
+          alt="U.S. Department of State seal"
+          className={ style.logo }
+          src="/dos-seal.png"
+        />
+        <strong>GPA Lab</strong>
+      </div>
     </Link>
+    <nav className={ style.nav }>
+      <Link href="/docs">
+        Documentation
+      </Link>
+    </nav>
   </div>
 );
 
