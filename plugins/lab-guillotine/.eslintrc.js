065cffccb3462f1f3f7adbde2366b18d7b8b5e94
mrewers <mrewers@users.noreply.github.com>
2021-01-26 15:01:42 -0500

    build: update to lastest ESLint config

--- plugins/lab-guillotine/.eslintrc.js
+++ plugins/lab-guillotine/.eslintrc.js
@@ -5,24 +5,6 @@ module.exports = {
   ],
   parser: 'babel-eslint',
   root: true,
-  rules: {
-    'react/jsx-pascal-case': ['error', { allowAllCaps: true }],
-    'node/no-missing-import': [
-      'error', {
-        allowModules: ['docs-hub', 'shared'],
-      },
-    ],
-    'node/no-unpublished-import': [
-      'error', {
-        allowModules: ['prop-types'],
-      },
-    ],
-    'node/no-unpublished-require': [
-      'error', {
-        allowModules: ['@wordpress/scripts'],
-      },
-    ],
-  },
   settings: {
     'import/resolver': {
       'babel-module': {
