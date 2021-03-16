a46e67eefdb70197353f30c88929dcecc20dfb5f
mrewers <hello@marek.dev>
2021-03-16 12:49:37 -0400

    feat(event): add spacer component to add padding between react components

--- plugins/lab-guillotine/custom-post-types/event/js/components/EventDocumentPanel.js
+++ plugins/lab-guillotine/custom-post-types/event/js/components/EventDocumentPanel.js
@@ -5,6 +5,7 @@ import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
 import MetaCheckbox from 'shared/components/MetaCheckbox';
 import MetaDateTimePicker from 'shared/components/MetaDateTimePicker';
 import MetaTextInput from 'shared/components/MetaTextInput';
+import Spacer from 'shared/components/Spacer';
 import { i18nize } from 'shared/utils/helpers';
 
 const DocsDocumentPanel = () => (
@@ -14,11 +15,13 @@ const DocsDocumentPanel = () => (
     <MetaDateTimePicker
       metaValue="_gpalab_event_date"
     />
+    <Spacer />
     <MetaTextInput
       label={ i18nize( 'Set the length of the event (in minutes)' ) }
       metaValue="_gpalab_event_duration"
       placeholder="60"
     />
+    <Spacer />
     <MetaCheckbox
       label={ i18nize( 'Show add to calendar button' ) }
       metaValue="_gpalab_event_show_atc"
